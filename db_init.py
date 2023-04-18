49f15dfdf32328949d6b926ac0cdf0740ba91184
BansheePrime <cmapuk-xpehob.yandex.ru>
2023-04-18 09:04:28 +0300

    adding tables to db

--- db_init.py
+++ db_init.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python3
-import sqlite3
-
-db_connection = sqlite3.connect("teachers.db")
-cursor = db_connection.cursor()
-
 '''
 teachers
 courses
@@ -11,3 +6,48 @@ streams
 grades
 '''
 
+import sqlite3
+# import csv
+
+db_connection = sqlite3.connect("teachers.db")
+cursor = db_connection.cursor()
+tables_list = []
+
+tb_teachers_query = '''CREATE TABLE teachers_tb (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        name TEXT NOT NULL,
+                        surname TEXT NOT NULL,
+                        email TEXT NOT NULL,
+                        );'''
+
+
+tb_courses_query = '''CREATE TABLE cources_tb (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        name TEXT NOT NULL UNIQUE,
+                        );'''
+
+tb_streams_query = '''CREATE TABLE streams_tb (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        course_id INTEGER NOT NULL,
+                        stream_number INTEGER NOT NULL UNIQUE,
+                        starting_date TEXT NOT NULL,
+                        students_number INTEGER NOT NULL,
+                        FOREIGN KEY (course_id) REFERENCES courses_tb(id)
+                        );'''
+
+tb_grades_query = '''CREATE TABLE grades (
+                        teacher_id INTEGER NOT NULL,
+                        stream_id INTEGER NOT NULL,
+                        grade REAL NOT NULL,
+                        PRIMARY KEY(teacher_id, stream_id),
+                        FOREIGN KEY (teacher_id) REFERENCES teachers_tb(id),
+                        FOREIGN KEY (stream_id) REFERENCES streams_tb(id)
+                        );'''
+
+
+cursor.executemany(tables_list)
+
+db_connection.close()
+
+# if __name__ == '__main__':
+#     main()
