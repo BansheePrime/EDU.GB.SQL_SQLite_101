d2b34168c562915b4c88e3c7fb9ba3decb08f608
BansheePrime <cmapuk-xpehob.yandex.ru>
2023-04-19 20:32:45 +0300

    adding ugly shell script for sqlite3 tables check

--- fast_check.sh
+++ fast_check.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 # Ограничение скрипта - названия таблиц должны совпадать с названиями ниже
-sqlite3 ./teachers.db <<EOF
-.tables
-.schema teachers_tb
-.schema cources_tb
-.schema streams_tb
-.schema grades_tb
-EOF
+sqlite3 ./teachers.db ".databases"
+table_1st=$(sqlite3 ./teachers.db ".tables" | awk '{ print $1 }')
+table_2nd=$(sqlite3 ./teachers.db ".tables" | awk '{ print $2 }')
+table_3rd=$(sqlite3 ./teachers.db ".tables" | awk '{ print $3 }')
+table_4th=$(sqlite3 ./teachers.db ".tables" | awk '{ print $4 }')
+sqlite3 ./teachers.db ".schema $table_1st"
+sqlite3 ./teachers.db ".schema $table_2nd"
+sqlite3 ./teachers.db ".schema $table_3rd"
+sqlite3 ./teachers.db ".schema $table_4th"
